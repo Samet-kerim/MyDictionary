4d5cc9f10fc078e122ff96dd49bce261ba113a33
Samet Kilic <180201024@uzem.education>
2021-09-08 22:00:06 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class MyDictionary<T, Y>
+    {
+        T[] tArray;
+        Y[] yArray;
+        T[] ttempArray;
+        Y[] ytempArray;
+
+        public MyDictionary()
+        {
+            tArray = new T[0];
+            yArray = new Y[0];
+        }
+
+        public void Ekle(T t,Y y)
+        {
+            ttempArray = tArray;
+            tArray = new T[tArray.Length + 1];
+            ytempArray = yArray;
+            yArray = new Y[yArray.Length + 1];
+            for (int i = 0; i < ttempArray.Length; i++)
+            {
+                tArray[i] = ttempArray[i];
+            }
+            tArray[tArray.Length - 1] = t;
+
+            for (int j = 0; j < ytempArray.Length; j++)
+            {
+                yArray[j] = ytempArray[j];
+            }
+            yArray[yArray.Length - 1] = y;
+
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Ekle(0, "Samet");
+            myDictionary.Ekle(1, "Nuriye");
+        }
+    }
+}
